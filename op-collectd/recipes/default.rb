58a0959566214a6f97a47c2635b66ae56103ff8b
Sammy Lin <sammylintw@gmail.com>
2015-04-14 18:05:27 +0800

    試試看

--- op-collectd/recipes/default.rb
+++ op-collectd/recipes/default.rb
@@ -7,34 +7,34 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file "#{Chef::Config[:file_cache_path]}/collectd-#{node["collectd"]["version"]}.tar.gz" do
-  source node["collectd"]["url"]
-  checksum node["collectd"]["checksum"]
-  action :create_if_missing
-end
+# remote_file "#{Chef::Config[:file_cache_path]}/collectd-#{node["collectd"]["version"]}.tar.gz" do
+#   source node["collectd"]["url"]
+#   checksum node["collectd"]["checksum"]
+#   action :create_if_missing
+# end
 
-bash "install-collectd" do
-  cwd Chef::Config[:file_cache_path]
-  code <<-EOH
-    tar -xzf collectd-#{node["collectd"]["version"]}.tar.gz
-    (cd collectd-#{node["collectd"]["version"]} && ./configure --prefix=#{node["collectd"]["dir"]} && make && make install)
-  EOH
-  not_if "#{node["collectd"]["dir"]}/sbin/collectd -h 2>&1 | grep #{node["collectd"]["version"]}"
-end
+# bash "install-collectd" do
+#   cwd Chef::Config[:file_cache_path]
+#   code <<-EOH
+#     tar -xzf collectd-#{node["collectd"]["version"]}.tar.gz
+#     (cd collectd-#{node["collectd"]["version"]} && ./configure --prefix=#{node["collectd"]["dir"]} && make && make install)
+#   EOH
+#   not_if "#{node["collectd"]["dir"]}/sbin/collectd -h 2>&1 | grep #{node["collectd"]["version"]}"
+# end
 
-template "/etc/collectd/collectd.conf" do
-  mode "0644"
-  source "collectd.conf.erb"
-  variables(
-    :name         => node["collectd"]["name"],
-    :dir          => node["collectd"]["dir"],
-    :interval     => node["collectd"]["interval"],
-    :read_threads => node["collectd"]["read_threads"]
-  )
-  notifies :restart, "service[collectd]"
-end
+# template "/etc/collectd/collectd.conf" do
+#   mode "0644"
+#   source "collectd.conf.erb"
+#   variables(
+#     :name         => node["collectd"]["name"],
+#     :dir          => node["collectd"]["dir"],
+#     :interval     => node["collectd"]["interval"],
+#     :read_threads => node["collectd"]["read_threads"]
+#   )
+#   notifies :restart, "service[collectd]"
+# end
 
-service "collectd" do
-  supports :status => true, :restart => true
-  action [ :enable, :start ]
-end
+# service "collectd" do
+#   supports :status => true, :restart => true
+#   action [ :enable, :start ]
+# end
